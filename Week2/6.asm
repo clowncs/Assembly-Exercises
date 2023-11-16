c8f8c7d71afc662655d3f8ef339921118e16ac5f
ClownCS <90112096+ClownCS@users.noreply.github.com>
2023-11-16 21:30:45 +0700

    Create 6.asm

---
+++ Week2/6.asm
@@ -0,0 +1,59 @@
+section .data
+text db 255 dup(0)
+len db 255
+len1 db 255 dup(0)
+final db 255 dup(0)
+
+
+section .text
+global _start
+
+_start:
+; Đọc số 1 từ người dùng
+mov eax, 3          
+mov ebx, 2           
+mov ecx, text 
+mov edx, len       
+int 0x80    
+call reversestring
+
+print:
+mov eax, 4
+mov ebx, 1
+mov ecx, final
+mov edx, 255
+int 0x80    
+jmp exit
+
+
+reversestring:
+mov ecx, 0
+find_length:
+    cmp byte [text + ecx], 0
+    je  end_find_length
+    inc ecx
+    jmp find_length
+end_find_length:
+    dec ecx
+    mov esi, text      
+    lea edi, [final + ecx]  
+reverse_loop:
+    cmp ecx, 0
+    jl end_reverse
+
+    mov al, [esi]
+    mov [edi], al
+
+    inc esi
+    dec edi
+    dec ecx
+
+    jmp reverse_loop
+
+end_reverse:
+ret
+
+exit:
+mov eax, 1            
+xor ebx, ebx         
+int 0x80
